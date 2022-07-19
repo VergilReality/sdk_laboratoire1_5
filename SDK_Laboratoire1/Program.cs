a1ba53cbc3b9e01384fe09309a8fb383e00ac918
DESKTOP-1VTIAP0\sofag <vergilreality@gmail.com>
2022-07-19 17:27:59 -0400

    Console Conversion Note

--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,23 +1,23 @@
-﻿////On roule l'application non-stop, tant que CTRL+C
-//using LibrairieConversionNote;
+﻿//On roule l'application non-stop, tant que CTRL+C
+using LibrairieConversionNote;
 
-//while (true)
-//{
-//    Console.Write("Type de conversion à faire : ");
-//    var input = Console.ReadLine();
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
 
-//    //Si l'utilisateur tape note
-//    if (input?.ToUpperInvariant() == "NOTE")
-//    {
-//        //On demande en boucle la conversion 
-//        while (true)
-//        {
-//            Console.Write("Conversion à faire : ");
-//            var note = Console.ReadLine();
-//            ConversionAlphabetiqueToNote conversion = new ConversionAlphabetiqueToNote();
-//            string noteRetour = conversion.Transformer(note);
-//            Console.Write(noteRetour);
-//            Console.WriteLine();
-//        }
-//    }
-//}
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            ConversionAlphabetiqueToNote conversion = new ConversionAlphabetiqueToNote();
+            string noteRetour = conversion.Transformer(note);
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
