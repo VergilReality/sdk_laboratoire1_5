1412fb9d7da077487b1c43e311844e7669ba3e60
DESKTOP-1VTIAP0\sofag <vergilreality@gmail.com>
2022-07-19 17:20:27 -0400

    librairie conversion alphabetic to note

---
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,46 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+    public string Transformer(string alpha)
+        {
+            alpha=alpha.ToUpperInvariant();
+            if (alpha == "A") 
+            {
+                return "LA";
+            }
+            else if (alpha == "B")
+            {
+                return "SI";
+            }
+            else if (alpha == "C")
+            {
+                return "DO";
+            }
+            else if (alpha == "D")
+            {
+                return "RE";
+            }
+            else if (alpha == "E")
+            {
+                return "MI";
+            }
+            else if (alpha == "F")
+            {
+                return "FA";
+            }
+            else if (alpha == "G")
+            {
+                return "SOL";
+            }
+            else
+            {
+                return "Choix invalide. Choissisez un lettre entre A et G.";
+            }
+            
+        }
+    }
+}
+
+
+
--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////On roule l'application non-stop, tant que CTRL+C
+//using LibrairieConversionNote;
+
+//while (true)
+//{
+//    Console.Write("Type de conversion à faire : ");
+//    var input = Console.ReadLine();
+
+//    //Si l'utilisateur tape note
+//    if (input?.ToUpperInvariant() == "NOTE")
+//    {
+//        //On demande en boucle la conversion 
+//        while (true)
+//        {
+//            Console.Write("Conversion à faire : ");
+//            var note = Console.ReadLine();
+//            ConversionAlphabetiqueToNote conversion = new ConversionAlphabetiqueToNote();
+//            string noteRetour = conversion.Transformer(note);
+//            Console.Write(noteRetour);
+//            Console.WriteLine();
+//        }
+//    }
+//}
